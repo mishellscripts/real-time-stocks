4999ca853363018a0da8ae64fbd3aecdd793c136
Michelle Song <michelle.song@sjsu.edu>
2017-07-09 16:39:28 -0700

    Drastically improved performance, bug fixes, and front-end changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,38 +7,5 @@ module.exports = app=>{
     app.route('/')
         .get((req, res, next)=>{
             res.render('index');
-        })
-        .post((req, res, next)=> {
-            https.get('https://www.quandl.com/api/v3/datasets/WIKI/' + req.body.ticker + '.json?api_key=' + process.env.QUANDL_API_KEY, (result)=> {
-                let data = '';    
-                result.on('data', chunk=> {
-                    data += chunk;
-                });
-                result.on('end', ()=> {
-                    data = JSON.parse(data);
-                    if (data.dataset) {
-                        Company.findOneAndUpdate({ticker_symbol: req.body.ticker.toUpperCase()}, {
-                            ticker_symbol: data.dataset.dataset_code.toUpperCase(),
-                            name: data.dataset.name,
-                            data_points: data.dataset.data
-                        }, {upsert: true}, err=> {
-                            if(err) console.log(err.message);
-                        });
-                    }
-			    });
-            });
-        });
-    app.route('/api/:ticker')
-        .get((req, res, next)=> {
-            Company.findOne({ticker_symbol: req.params.ticker.toUpperCase()}, (err, company)=> {
-                if(err) console.log(err.message);
-                res.send(company);
-            });
-        });
-    app.route('/all')
-        .get((req, res, next)=> {
-            Company.find({}, function(err, data) {
-				res.send(data);
-			});
         });
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (()=> {
-    let app = angular.module('stocks', []);
+    let app = angular.module('stocks', ['ngAnimate']);
 
     app.controller('StockController', ['$scope', '$http',
         ($scope, $http)=> {
@@ -8,34 +8,34 @@
 
             $scope.addCompany = ()=> {
                 const newCompanySymbol = $scope.tickerSymbol;
+                // Exit out if no user input
                 if (!newCompanySymbol) return;
                 
                 let newCompany = {
-                    ticker_symbol: newCompanySymbol
+                    ticker_symbol: newCompanySymbol.toUpperCase()
                 }
 
                 let apiKey = '';
+                // Get API key from the server
                 $.get('/rest/getenv', data=> {
-                    apiKey = data;
+                    apiKey = data.result;
                 })
 
-                $.get('/api/' + newCompanySymbol, data =>{
-                    if (!data) {
-                        $http.get('https://www.quandl.com/api/v3/datasets/WIKI/' + newCompanySymbol + '.json?api_key=' + apiKey)
-                            .then(response=>{
-                                newCompany.name = response.data.dataset.name;
-                            });
-                    } else {
-                        newCompany.name = data.name;
-                    }
-                });
+                $http.get('https://www.quandl.com/api/v3/datasets/WIKI/' + newCompanySymbol + '.json?api_key=' + apiKey)
+                    .then(response=>{
+                        newCompany.name = response.data.dataset.name;
+                        // Compare objects works after removing $$hashkey property
+                        if (angular.toJson($scope.companies).indexOf(angular.toJson(newCompany)) == -1) {
+                            $scope.companies.push(newCompany);
+                        }
+                        // Todo: Graph on chart
+                        // Todo: If already exists, replace data in chart
+                    }, err=> {
+                        // If ticker symbol doesn't exist, display a message on the front end and exit out of addCompany function
+                        displayError("Ticker symbol not found");
+                    });
 
-                // Add company to array if not already there
-                if ($scope.companies.indexOf(newCompany) == -1)
-                    $scope.companies.push(newCompany);
-                
-                $scope.tickerSymbol = '';
-                console.log($scope.companies);
+                $scope.tickerSymbol = '';         
             }    
 
             $scope.removeCompany = company=> { 
@@ -46,16 +46,14 @@
     ]);
 })();
 
-$('#stock-form').submit(function(e){
-    e.preventDefault();
-    $.ajax({
-        url:'/',
-        type:'post',
-        data:$('#stock-form').serialize(),
-        success:function(){
-            //whatever you wanna do after the form is successfully submitted
-        }
-    });
-});
+const displayError = message=> {
+    $('#new-stock').append('<div class="error" style="display: none">' + message + '</div>');
+        $('.error').fadeIn('fast', ()=> {
+            $('.error').delay(1000).fadeOut('fast', ()=> {
+                $('.error').remove();
+            });
+        });
+}
+$('.company').fadeIn('slow');
 
 var socket = io();
