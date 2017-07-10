a11a5af202da66dc5c69d7aefa671a8b738254fc
Michelle Song <michelle.song@sjsu.edu>
2017-07-09 17:41:54 -0700

    More front-end changes. Added highchart

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 (()=> {
     let app = angular.module('stocks', ['ngAnimate']);
 
+    let apiKey = '';
+    // Get API key from the server
+    $.get('/rest/getenv', data=> {
+        apiKey = data.result;
+    })
+
     app.controller('StockController', ['$scope', '$http',
         ($scope, $http)=> {
             $scope.tickerSymbol = '';
@@ -15,12 +21,6 @@
                     ticker_symbol: newCompanySymbol.toUpperCase()
                 }
 
-                let apiKey = '';
-                // Get API key from the server
-                $.get('/rest/getenv', data=> {
-                    apiKey = data.result;
-                })
-
                 $http.get('https://www.quandl.com/api/v3/datasets/WIKI/' + newCompanySymbol + '.json?api_key=' + apiKey)
                     .then(response=>{
                         newCompany.name = response.data.dataset.name;
@@ -29,10 +29,11 @@
                             $scope.companies.push(newCompany);
                         }
                         // Todo: Graph on chart
+                        plotChart(response.data.dataset.name, response.data.dataset.data);
                         // Todo: If already exists, replace data in chart
                     }, err=> {
                         // If ticker symbol doesn't exist, display a message on the front end and exit out of addCompany function
-                        displayError("Ticker symbol not found");
+                        if (err.status === 404) displayError("Ticker symbol not found");
                     });
 
                 $scope.tickerSymbol = '';         
@@ -54,6 +55,65 @@ const displayError = message=> {
             });
         });
 }
+
+const plotChart = (name, data)=> {
+    Highcharts.chart('chart', {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: name
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: 'Volume'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'area',
+            name: name,
+            data: data
+        }]
+    });
+};
+
 $('.company').fadeIn('slow');
 
 var socket = io();
